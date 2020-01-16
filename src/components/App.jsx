d489dd88045c9aafd492cec85d57427a38ab0a08
Student <student@Students-MacBook-Air.local>
2020-01-15 18:31:17 -0800

    Components and css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import Answer from './Answer';
+import Question from './Question';
 // import components
 
 class App extends Component {
-
+  var QnA = [];
+  for(var i = 0; i < 6; i++){
+    var index = Math.floor(Math.random() * 6);
+  }
   render() {
     return (
       <div className="app">
-        
+        <Answer />
+        <Question />
         
       </div>
     );
